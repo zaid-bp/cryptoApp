b4dbf4f85d9e85f66699bcabe8200cff4ff77269
zaid-bp <zaid@bitspro.com>
2023-08-18 19:10:46 +0500

    newcommit

--- src/Components/coins.tsx
+++ src/Components/coins.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../features/store'
 
@@ -7,17 +6,21 @@ function CoinCard() {
 const { coinData } = useSelector((state:RootState)=>state.coin)
   return (
     <div className=''>{
-    coinData.map((data)=>{
-        const {id, symbol, name, image, market_data{current_price}} = data
+    coinData.map((data:any)=>{
+        const {id, symbol, name, image, market_data} = data;
+        const {current_price:{usd}, market_cap_change_percentage_24h: mccp24h, market_cap_rank:rank} = market_data
         
         return(
             <div className='flex bg-gray-300 m-5 p-3 w-60 rounded-md' key={id}>
-                {/* <img src={image.thumb} alt="failed" /> */}
+                <p>{rank}</p>
                 <img src={image.small} alt="failed" />
-                {/* <img src={image.large} alt="failed" /> */}
                 <div className='ml-3'>
                     <h6 className='font-bold text-start'>{symbol.toUpperCase()}</h6>
                     <span>{name}</span>
+                </div>
+                <div className='ml-auto text-right'>
+                    <span className='block'>${usd}</span>
+                    <span className={`${mccp24h > 0 ? 'text-green-700':'text-red-700'}`}>{mccp24h}</span>
                 </div>
             </div>
         )
