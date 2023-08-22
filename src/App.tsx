9a45b0a2f3eff70c528b2d960c7243eb9692fad3
zaid-bp <zaid@bitspro.com>
2023-08-22 11:59:32 +0500

    error resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import './App.css'
 import { useEffect } from 'react'
-import { fetchData, fetchTrendings } from './features/slices/coinSlice'
+import { fetchData } from './features/slices/coinSlice'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from './features/store'
 import ListCoins from './Components/listCoins'
-import CoinCard from './Components/coins'
 
 function App() {
 const {isLoading, isError} = useSelector((state:RootState)=>state.coin)
 const dispatch = useDispatch()
 useEffect(()=>{
   dispatch(fetchData() as any)
-  dispatch(fetchTrendings() as any)
 },[dispatch])
 if(isLoading){
   return <p>Loading...</p>
@@ -21,7 +19,6 @@ if(isError){
 }
   return (
     <>
-    <CoinCard /> 
     <ListCoins />
     </>
   )
